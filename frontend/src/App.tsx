ebfe169fec4b8a8335a39ebfd631a5938f8e65e4
God-yash12 <gannuthapa986@gmail.com>
2025-05-20 16:35:54 +0545

    project  setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import RegistrationForm from "./components/RegistrationForm"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <RegistrationForm />
+      </div>
+
+    </>
+  )
+}
+
+export default App
